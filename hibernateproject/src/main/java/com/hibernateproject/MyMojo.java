21203a1cc67523ba8e58314081c248e07658a9f5
Nick Kraftor <kraftmatic@gmail.com>
2015-03-16 23:48:02 -0500

    no message

--- hibernateproject/src/main/java/com/hibernateproject/MyMojo.java
+++ hibernateproject/src/main/java/com/hibernateproject/MyMojo.java
@@ -24,7 +24,7 @@ public class MyMojo
      * @required
      */
     private File outputDirectory;
-    static final Logger LOGGER = Logger.getLogger(MyMojo.class);
+    private static final Logger LOGGER = Logger.getLogger(MyMojo.class);
 
     public void execute()
         throws MojoExecutionException
@@ -59,7 +59,7 @@ public class MyMojo
                 }
                 catch ( IOException e )
                 {
-                    LOGGER.warn("Could not write to file");
+                    LOGGER.warn(e);
                 }
             }
         }
